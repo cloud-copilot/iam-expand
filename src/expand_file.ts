ee1d37c876bd37199630cb069f377fe5d486aaf7
David Kerber <dave@kerber.io>
2024-09-15 16:38:53 -0500

    Modifying expand-json function to modify document in place to save memory.

--- src/expand_file.ts
+++ src/expand_file.ts
@@ -1,39 +1,38 @@
-import { expandIamActions, ExpandIamActionsOptions } from "./expand.js"
+import { expandIamActions, ExpandIamActionsOptions } from "./expand.js";
 
 /**
- * Takes any JSON document and expands any Action found in the document
+ * Takes any JSON document and expands any Action or NotAction string or array of strings in the document.
+ * *MODIFIES THE DOCUMENT IN PLACE*
  *
  * @param options the options to use when expanding the actions
  * @param document the JSON document to expand
  * @param key the key of the current node in the document
- * @returns the expanded JSON document
+ * @returns the original JSON document with any actions expanded in place
  */
 export async function expandJsonDocument(options: Partial<ExpandIamActionsOptions>, document: any, key?: string): Promise<any> {
-  if(key === 'Action' || key === 'NotAction') {
-    if(typeof document === 'string') {
-      return await expandIamActions(document, options)
+  if (key === 'Action' || key === 'NotAction') {
+    if (typeof document === 'string') {
+      return expandIamActions(document, options);
     }
-    if(Array.isArray(document) && document.length > 0 && typeof document[0] === 'string') {
-      const value =  await expandIamActions(document, {...options})
-      return value
+    if (Array.isArray(document) && document.length > 0 && typeof document[0] === 'string') {
+      return expandIamActions(document, options);
     }
   }
 
-  if(Array.isArray(document)) {
-    return Promise.all(document.map(async (item) => {
-      return expandJsonDocument(options, item)
-    }))
-  }
-
-  if(typeof document === 'object') {
-    const keys = Object.keys(document)
-    const newObject: any = {}
-    for(const key of keys) {
-      const value = document[key]
-      newObject[key] = await expandJsonDocument(options, value, key)
+  if (Array.isArray(document)) {
+    const results = [];
+    for (const item of document) {
+      results.push(await expandJsonDocument(options, item));
     }
-    return newObject
+    return results;
   }
 
-  return document
+  if (typeof document === 'object' && document !== null) {
+    for (const key of Object.keys(document)) {
+      document[key] = await expandJsonDocument(options, document[key], key);
+    }
+    return document;
+  }
+
+  return document;
 }
