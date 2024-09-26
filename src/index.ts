b7f1a9d31bdb25f947adfd3571c4b148e5224d60
David Kerber <dave@kerber.io>
2024-09-25 21:50:45 -0500

    Exporting additional functions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export * from './expand.js';
+export { extractActionsFromLineOfInput } from './cli_utils.js';
+export { ExpandIamActionsOptions, InvalidActionBehavior, expandIamActions } from './expand.js';
+export { expandJsonDocument } from './expand_file.js';
+
