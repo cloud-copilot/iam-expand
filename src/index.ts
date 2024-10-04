adbc5af83e620832e327a556c4a122dc83c0be2b
David Kerber <dave@kerber.io>
2024-10-04 14:48:53 -0500

    Fixing export name for invertIamActions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { extractActionsFromLineOfInput } from './cli_utils.js';
 export { expandIamActions, type ExpandIamActionsOptions, type InvalidActionBehavior } from './expand.js';
 export { expandJsonDocument, type ExpandJsonDocumentOptions } from './expand_file.js';
-export { invertIamActions as invert } from './invert.js';
+export { invertIamActions } from './invert.js';
 
